942246e20f963018a8831ed3db9f289e29e5e3be
root <root@localhost.localdomain>
2017-07-08 23:11:38 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ FROM nothingdocker/centos-node
 #RUN yum install -y make gcc gcc-c++
 #RUN npm install -g node-gyp
 RUN npm install -g --unsafe-perm sinopia
-
+COPY config.yaml ~/.config/sinopia/config.yaml
 ENTRYPOINT ["/entrypoint.sh"]
 CMD ["/usr/sbin/init"]
--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ REPO=nothingdocker
 IMAGE_NAME=`basename $PWD`
 DOCKER_NAME=${1:-demo}
 docker rm -f $DOCKER_NAME
-docker run --privileged \
+docker run -d --privileged \
 	--name $DOCKER_NAME \
 	-v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 	-v /data:/data \
 	-p 4873:4873 \
 	$REPO/$IMAGE_NAME
-#docker exec -it $DOCKER_NAME bash
+docker exec -it $DOCKER_NAME bash
